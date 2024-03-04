b600d442ecc81e43c90fe999d66bfc7903e8ef97
Kwan Yeob Jung <ky@Kwans-MBP.hitronhub.home>
2024-03-04 14:05:14 -0500

    Created a new cleansed datase

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 aud_usd_csv = pd.read_csv("/Users/ky/PycharmProjects/fx_swap_prediction/dataset/AUD:USD.csv")
 jyp_usd_csv = pd.read_csv("/Users/ky/PycharmProjects/fx_swap_prediction/dataset/JYP:USD.csv")
@@ -45,6 +46,7 @@ for i in range(0, len(jyp_rate_list)):
     if jyp_rate_list[i] == 'ND':
         jyp_rate_list[i] = jyp_rate_list[i-1]
 
+
 # check if null value has been successfully replaced.
 
 if 'ND' in aud_rate_list:
@@ -57,12 +59,35 @@ if 'ND' in jyp_rate_list:
 else:
     print("No")
 
+# assign the list to the proper columns.
+aud_usd_csv ['Rate'] = aud_rate_list
+jyp_usd_csv ['Rate'] = jyp_rate_list
+
+# now change the columns into correct data format. Rate ( string -> integer), Date (string -> dataframe)
+
+aud_usd_csv['Rate'] = pd.to_numeric(aud_usd_csv['Rate'])
+jyp_usd_csv['Rate'] = pd.to_numeric(jyp_usd_csv['Rate'])
+
+aud_usd_csv['Date'] = pd.to_datetime(aud_usd_csv['Date'])
+jyp_usd_csv['Date'] = pd.to_datetime(jyp_usd_csv['Date'])
+
+
 # Is it worth it to remove all extreme outliers? It may be critical to have that values accountable since those are good factors from
 # the market that actually happened.
 
 # It requires further investigations. But for now, let's let as it is and then this can be a changing factor for later.
 
 # This is relatively clean data from Board of Governors of the Federal Reserve System so let's move on with EDA
+
+
+#make a new datasets
+
+#aud_usd_csv.to_csv('/Users/ky/PycharmProjects/fx_swap_prediction/dataset/aud_usd_new_dataset.csv', sep=str(','), encoding='utf-8',index = False)
+#jyp_usd_csv.to_csv('/Users/ky/PycharmProjects/fx_swap_prediction/dataset/jyp_usd_new_dataset.csv', sep=str(','), encoding='utf-8',index = False)
+
+
+
+
 
 
 
